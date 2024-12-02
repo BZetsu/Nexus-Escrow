e07b61f53ef6fb5afe4aed40788fb626e8147ecf
JBZetsu <nwankwojames7@gmail.com>
2024-12-02 02:13:40 +0100

    fix: increase z-index for profile dropdown menu to ensure proper layering

--- components/Navbar/TopNavbar.tsx
+++ components/Navbar/TopNavbar.tsx
@@ -279,8 +279,9 @@ export default function Example() {
                       leave="transition ease-in duration-75"
                       leaveFrom="transform opacity-100 scale-100"
                       leaveTo="transform opacity-0 scale-95"
+                      className="z-[9999999] relative"
                     >
-                      <Menu.Items className="absolute right-0 z-50 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Items className="absolute right-0 z-[9999999] mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                         <Menu.Item>
                           {
                           () => (
